f0650af5643be43a9036534158fef6842bb523c0
Chofi <fran.h.sofia@gmail.com>
2026-04-08 23:22:09 -0300

    PaginaBienvenidaActualizada.favicon

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,70 +1,64 @@
-// import logoDark from "./logo-dark.svg";
-// import logoLight from "./logo-light.svg";
 import { Link } from "react-router";
 
 export function Welcome() {
+  const actualYear = new Date().getFullYear();
+
   return (
-    <main className="flex items-center justify-center pb-4 bg-white">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0 w-full">
-        <header className="flex flex-row items-center gap-9 w-full">
+    <body className="flex flex-col h-full w-full">
+        <header className="relative row items-center gap-9">
           <nav className="flex w-full">
-            <div className="flex justify-center items-center bg-slate-800 h-10 w-full gap-8">
-              <Link to="/" className="text-orange-300 hover:underline hover:text-orange-200/70">Inicio</Link>
-              <Link to="/login" className="text-orange-300 hover:underline hover:text-orange-200/70">Iniciar sesión</Link>
-              <Link to="/register" className="text-orange-300 hover:underline hover:text-orange-200/70">Crear cuenta</Link>
+            <div className="flex justify-end items-center bg-[#11332C] h-10 w-full gap-8 pr-4">
+              <Link to="/" className="text-[#F2B11C] hover:underline hover:text-[#F2B11C]/70">Inicio</Link>
+              <Link to="#aboutUs" className="text-[#F2B11C] hover:underline hover:text-[#F2B11C]/70">Nosotros</Link>
+              <Link to="#history" className="text-[#F2B11C] hover:underline hover:text-[#F2B11C]/70">Historia</Link>
+              <Link to="/login" className="text-[#F2B11C] hover:underline hover:text-orange-200/70">Iniciar sesión</Link>
+              <Link to="/register" className="bg-[#F2B11C] border rounded-4xl p-1 hover:bg-[#FCF9F6] hover:text-[#F2B11C]">Crear cuenta</Link>
             </div>
           </nav>
         </header>
-        <div className="max-w-75 w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-black text-center">
-              PROXIMAMENTE MALBEC CONNECTED...
-            </p>
-          </nav>
-        </div>
-      </div>
-    </main>
-  );
-}
 
-const resources = [
-  {
-    href: "https://reactrouter.com/docs",
-    text: "React Router Docs",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://rmx.as/discord",
-    text: "Join Discord",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
-  },
-];
+        <main className="flex flex-col items-center justify-center pb-4 gap-6 bg-[#FCF9F6]">
+
+          <section>
+            <img src="/HAhX_ECacAIeUca.png" alt="" className="w-full h-192"/>
+          </section>
+
+          <section id="aboutUs" className="flex flex-col w-[70%] pb-4 border border-b-amber-400">
+            <h1 className="text-[#F2B11C] text-center underline text-4xl mb-4">Acerca de Nosotros</h1>
+            <div className="grid grid-cols-2 mt-3 gap-4">
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ipsam obcaecati. Perferendis ex, ducimus eum veniam fugit natus nulla voluptate quis a, consequatur deleniti voluptatibus reprehenderit tempora cupiditate eius fugiat.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis doloribus maxime facere exercitationem totam unde odit quisquam, neque tenetur ipsam quis quam tempora hic soluta modi deleniti? Placeat, doloremque rem.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod debitis, dolores nemo magnam quae hic iste? Inventore voluptatum quis perspiciatis itaque consequuntur obcaecati quae iure nobis at tempora? Voluptates, veniam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sunt cumque, eaque impedit necessitatibus perspiciatis totam aspernatur quaerat blanditiis saepe esse accusamus fuga ea nam cum, quo provident et. Iure?
+              </p>
+              <img src="/image.webp" alt="imagen1" />
+            </div>
+          </section>
+
+          <section id="history" className="flex flex-col w-[70%] pb-4 border border-b-amber-400">
+            <h1 className="text-[#F2B11C] text-center underline text-4xl mb-4">Nuestra Historia</h1>
+            <div className="grid grid-cols-2 mt-3 gap-4">
+              <img src="/image.webp" alt="imagen1" />
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ipsam obcaecati. Perferendis ex, ducimus eum veniam fugit natus nulla voluptate quis a, consequatur deleniti voluptatibus reprehenderit tempora cupiditate eius fugiat.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis doloribus maxime facere exercitationem totam unde odit quisquam, neque tenetur ipsam quis quam tempora hic soluta modi deleniti? Placeat, doloremque rem.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod debitis, dolores nemo magnam quae hic iste? Inventore voluptatum quis perspiciatis itaque consequuntur obcaecati quae iure nobis at tempora? Voluptates, veniam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sunt cumque, eaque impedit necessitatibus perspiciatis totam aspernatur quaerat blanditiis saepe esse accusamus fuga ea nam cum, quo provident et. Iure?
+              </p>
+            </div>
+          </section>
+
+        </main>
+
+        <footer className="py-4 bg-[#11332C]">
+          <div className="flex flex-row justify-center items-center">
+            <p className="text-[#F2B11C] mx-2 text-sm">
+              &copy;&nbsp; {actualYear}&nbsp; Malbec Connected
+            </p>
+            <p className="text-[#F2B11C] mx-2 text-sm">Todos los derechos reservados</p>
+          </div>
+        </footer>
+    </body>
+  )
+}
